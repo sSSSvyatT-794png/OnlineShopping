461194cd3da25946f4c89ed52260016d55627a01
sSSSvyatT-794png <62217040+sSSSvyatT-794png@users.noreply.github.com>
2020-05-18 13:29:26 +0300

    Добавьте файлы проекта.

---
+++ OnlineShopping/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using OnlineShopping.DAL;
+using OnlineShopping.Filters;
+using OnlineShopping.Repository;
+using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    [FrontPageActionFilter]
+    public class HomeController : Controller
+    {
+        #region Other Class references ... // Instance on Unit of Work 
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        #endregion
+        /// <summary>
+        /// Home Page
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.FeaturedProducts = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetListByParameter(i => i.IsFeatured == true && i.IsDelete == false && i.IsActive == true).ToList();
+            return View();
+        }
+
+        #region Disposing UnitOfWork Context ...
+        protected override void Dispose(bool disposing)
+        {
+            _unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        public ActionResult Profile()
+        {
+            return PartialView("Profile");
+        }
+
+        public ActionResult Help()
+        {
+            return PartialView("Help");
+        }
+    }
+}
